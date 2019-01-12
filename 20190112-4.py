4c451a1dc9863da955f39377473856b06eb2b080
elain <elain.lizhe@gmail.com>
2019-01-12 22:17:01 +0100

    update 跃火林中

--- 20190112-4.py
+++ 20190112-4.py
@@ -5,13 +5,13 @@ while True:
         pos = evilstone.pos
         if pos.x == 34:     # == means "is equal to"
             # 如果恶魔石在左边，走到右边。
-            hero.say("右边")
+            hero.moveXY(46, 22)
             pass
         else:
             # 如果恶魔石在右边，走到左边。
-            hero.say("左边")
+            hero.moveXY(34, 22)
             pass
     else:
         # 如果没有恶魔石，那就去到中间。
-        hero.say("中间")
+        hero.moveXY(40, 22)
         pass
