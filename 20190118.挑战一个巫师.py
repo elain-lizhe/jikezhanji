c0da378baf107ce5c81e4509e290e54a8b689a0c
elain <elain.lizhe@gmail.com>
2019-01-21 21:41:41 +0100

    update wushi

--- 20190118.挑战一个巫师.py
+++ 20190118.挑战一个巫师.py
@@ -1,10 +1,34 @@
 # 尽可能多地打败食人魔。
 # 使用'cast'和'canCast' 来表示法术。
 
+
 while True:
-    if hero.canCast("lightning-bolt", hero.findNearestEnemy()):
-        hero.cast("lightning-bolt", hero.findNearestEnemy())
-    elif hero.canCast("chain-lightning", hero.findNearestEnemy()):
-        hero.cast("chain-lightning", hero.findNearestEnemy())
+    enemy = hero.findNearestEnemy()
+    if not enemy:
+        pass
+    elif hero.canCast("lightning-bolt", enemy):
+        hero.cast("lightning-bolt", enemy)
+    elif hero.canCast("chain-lightning", enemy):
+        hero.cast("chain-lightning", enemy)
+    elif enemy.type == "ogre":
+        hero.moveXY(10, 44) 
     else:
-        hero.cast("regen", hero.findNearestEnemy())
+        hero.cast("regen", hero)
+    
+
+while True:
+    enemy = hero.findNearestEnemy()
+    if enemy:
+        # hero.attack(enemy)
+        hero.cleave(enemy)
+    else:
+
+
+
+item = hero.findNearestItem()
+if item:
+    # 移动到物品的位置。
+    pos = item.pos
+    x = pos.x
+    y = pos.y
+    hero.moveXY(x, y)
