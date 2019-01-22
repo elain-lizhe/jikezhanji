d89de97cbb56c1adb9ddc191831bce7bc91328e9
elain <elain.lizhe@gmail.com>
2019-01-22 10:59:24 +0100

    20190118 挑战一个巫师.py

--- 20190118.挑战一个巫师.py
+++ 20190118.挑战一个巫师.py
@@ -32,3 +32,54 @@ if item:
     x = pos.x
     y = pos.y
     hero.moveXY(x, y)
+
+
+
+
+    # 尽可能多地打败食人魔。
+# 使用'cast'和'canCast' 来表示法术。
+while True:
+    if hero.canCast("regen", hero):
+        hero.cast("regen", hero)
+        
+    enemy = hero.findNearestEnemy()
+    if enemy:
+        if enemy.type == "ogre" and hero.canCast("lightning-bolt", enemy):
+            hero.say("ogre")
+            hero.cast("lightning-bolt", enemy)
+            continue
+        distance = hero.distanceTo(enemy)
+        hero.say(distance)
+        if distance < 3.18:            
+            if hero.canCast("chain-lightning", enemy):
+                hero.cast("chain-lightning", enemy)
+                continue
+            hero.attack(enemy)
+        if 3.18 < distance and distance <= 30:
+            hero.attack(enemy)
+        if distance > 30:
+            pass
+           # hero.moveXY(10, 44)# 尽可能多地打败食人魔。
+# 使用'cast'和'canCast' 来表示法术。
+while True:
+    if hero.canCast("regen", hero):
+        hero.cast("regen", hero)
+        
+    enemy = hero.findNearestEnemy()
+    if enemy:
+        if enemy.type == "ogre" and hero.canCast("lightning-bolt", enemy):
+            hero.say("ogre")
+            hero.cast("lightning-bolt", enemy)
+            continue
+        distance = hero.distanceTo(enemy)
+        hero.say(distance)
+        if distance < 3.18:            
+            if hero.canCast("chain-lightning", enemy):
+                hero.cast("chain-lightning", enemy)
+                continue
+            hero.attack(enemy)
+        if 3.18 < distance and distance <= 30:
+            hero.attack(enemy)
+        if distance > 30:
+            pass
+           # hero.moveXY(10, 44)
